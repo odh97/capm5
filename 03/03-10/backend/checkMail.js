11e1a6e8dd61b47b36d2ac4f4379cca9ecbd9993
Oh Daehyeon <xzdheogus1@gmail.com>
2024-10-03 23:12:57 +0900

    fix

---
+++ 03/03-10/backend/checkMail.js
@@ -0,0 +1,40 @@
+import nodemailer from 'nodemailer';
+
+export function checkEmail(email) {
+  if (email.includes('@') && email.includes('.')) {
+    return true;
+  } else {
+    console.log('이메일 형식이 아닙니다.');
+    return false;
+  }
+}
+
+export function getWelcomeMailTemplate(name) {
+  return `
+    <html>
+        <body>
+            <h1>환영합니다! ${name}님</h1>
+            <p>회원 가입을 축하합니다.</p>
+        </body>
+    </html>
+  `;
+}
+
+export async function sendTemplateMail(myEmail, template) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'test@gmail.com',
+      pass: 'test', // gmail 앱 비밀번호를 만들어서 적용 해봤음.
+    },
+  });
+
+  const mailResult = await transporter.sendMail({
+    from: 'test@gmail.com',
+    to: myEmail,
+    subject: '가입을 환영합니다!',
+    html: template,
+  });
+
+  console.log(mailResult);
+}
---
+++ 04/04-01-docker/index.js
@@ -0,0 +1,3 @@
+console.log('이 파일은 도커 컨테이너에서 실행됩니다.');
+
+while (true) {}
